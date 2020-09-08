70b6740ce939f93a00c1d2cd01f807edd9609213
Siva <byteshiva@gmail.com>
2020-09-08 20:16:47 +0530

    updated gen file and module files

--- addition/addition.pb.go
+++ addition/addition.pb.go
@@ -2,12 +2,16 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.6.1
-// source: addition/addition.proto
+// source: addition.proto
 
-package simplegrpc
+package addition
 
 import (
+	context "context"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -37,7 +41,7 @@ type AddRequest struct {
 func (x *AddRequest) Reset() {
 	*x = AddRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_addition_addition_proto_msgTypes[0]
+		mi := &file_addition_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -50,7 +54,7 @@ func (x *AddRequest) String() string {
 func (*AddRequest) ProtoMessage() {}
 
 func (x *AddRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_addition_addition_proto_msgTypes[0]
+	mi := &file_addition_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -63,7 +67,7 @@ func (x *AddRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use AddRequest.ProtoReflect.Descriptor instead.
 func (*AddRequest) Descriptor() ([]byte, []int) {
-	return file_addition_addition_proto_rawDescGZIP(), []int{0}
+	return file_addition_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *AddRequest) GetNumber() int32 {
@@ -91,7 +95,7 @@ type AddResponse struct {
 func (x *AddResponse) Reset() {
 	*x = AddResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_addition_addition_proto_msgTypes[1]
+		mi := &file_addition_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -104,7 +108,7 @@ func (x *AddResponse) String() string {
 func (*AddResponse) ProtoMessage() {}
 
 func (x *AddResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_addition_addition_proto_msgTypes[1]
+	mi := &file_addition_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -117,7 +121,7 @@ func (x *AddResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use AddResponse.ProtoReflect.Descriptor instead.
 func (*AddResponse) Descriptor() ([]byte, []int) {
-	return file_addition_addition_proto_rawDescGZIP(), []int{1}
+	return file_addition_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *AddResponse) GetSum() int64 {
@@ -127,46 +131,43 @@ func (x *AddResponse) GetSum() int64 {
 	return 0
 }
 
-var File_addition_addition_proto protoreflect.FileDescriptor
+var File_addition_proto protoreflect.FileDescriptor
 
-var file_addition_addition_proto_rawDesc = []byte{
-	0x0a, 0x17, 0x61, 0x64, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x61, 0x64, 0x64, 0x69, 0x74,
-	0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08, 0x61, 0x64, 0x64, 0x69, 0x74,
-	0x69, 0x6f, 0x6e, 0x22, 0x4a, 0x0a, 0x0a, 0x41, 0x64, 0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
-	0x74, 0x12, 0x16, 0x0a, 0x06, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x05, 0x52, 0x06, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x12, 0x24, 0x0a, 0x0d, 0x61, 0x6e, 0x6f,
-	0x74, 0x68, 0x65, 0x72, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05,
-	0x52, 0x0d, 0x61, 0x6e, 0x6f, 0x74, 0x68, 0x65, 0x72, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x22,
-	0x1f, 0x0a, 0x0b, 0x41, 0x64, 0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x10,
-	0x0a, 0x03, 0x73, 0x75, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x03, 0x73, 0x75, 0x6d,
-	0x32, 0x40, 0x0a, 0x08, 0x41, 0x64, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x34, 0x0a, 0x03,
-	0x41, 0x64, 0x64, 0x12, 0x14, 0x2e, 0x61, 0x64, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x41,
-	0x64, 0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x15, 0x2e, 0x61, 0x64, 0x64, 0x69,
-	0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x41, 0x64, 0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
-	0x22, 0x00, 0x42, 0x2c, 0x5a, 0x2a, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
-	0x2f, 0x62, 0x79, 0x74, 0x65, 0x73, 0x68, 0x69, 0x76, 0x61, 0x2f, 0x73, 0x69, 0x6d, 0x70, 0x6c,
-	0x65, 0x67, 0x72, 0x70, 0x63, 0x3b, 0x73, 0x69, 0x6d, 0x70, 0x6c, 0x65, 0x67, 0x72, 0x70, 0x63,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_addition_proto_rawDesc = []byte{
+	0x0a, 0x0e, 0x61, 0x64, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x12, 0x08, 0x61, 0x64, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x4a, 0x0a, 0x0a, 0x41, 0x64,
+	0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x6e, 0x75, 0x6d, 0x62,
+	0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x06, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72,
+	0x12, 0x24, 0x0a, 0x0d, 0x61, 0x6e, 0x6f, 0x74, 0x68, 0x65, 0x72, 0x4e, 0x75, 0x6d, 0x62, 0x65,
+	0x72, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x0d, 0x61, 0x6e, 0x6f, 0x74, 0x68, 0x65, 0x72,
+	0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x22, 0x1f, 0x0a, 0x0b, 0x41, 0x64, 0x64, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x10, 0x0a, 0x03, 0x73, 0x75, 0x6d, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x03, 0x52, 0x03, 0x73, 0x75, 0x6d, 0x32, 0x40, 0x0a, 0x08, 0x41, 0x64, 0x64, 0x69, 0x74,
+	0x69, 0x6f, 0x6e, 0x12, 0x34, 0x0a, 0x03, 0x41, 0x64, 0x64, 0x12, 0x14, 0x2e, 0x61, 0x64, 0x64,
+	0x69, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x41, 0x64, 0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
+	0x1a, 0x15, 0x2e, 0x61, 0x64, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x41, 0x64, 0x64, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
-	file_addition_addition_proto_rawDescOnce sync.Once
-	file_addition_addition_proto_rawDescData = file_addition_addition_proto_rawDesc
+	file_addition_proto_rawDescOnce sync.Once
+	file_addition_proto_rawDescData = file_addition_proto_rawDesc
 )
 
-func file_addition_addition_proto_rawDescGZIP() []byte {
-	file_addition_addition_proto_rawDescOnce.Do(func() {
-		file_addition_addition_proto_rawDescData = protoimpl.X.CompressGZIP(file_addition_addition_proto_rawDescData)
+func file_addition_proto_rawDescGZIP() []byte {
+	file_addition_proto_rawDescOnce.Do(func() {
+		file_addition_proto_rawDescData = protoimpl.X.CompressGZIP(file_addition_proto_rawDescData)
 	})
-	return file_addition_addition_proto_rawDescData
+	return file_addition_proto_rawDescData
 }
 
-var file_addition_addition_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_addition_addition_proto_goTypes = []interface{}{
+var file_addition_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_addition_proto_goTypes = []interface{}{
 	(*AddRequest)(nil),  // 0: addition.AddRequest
 	(*AddResponse)(nil), // 1: addition.AddResponse
 }
-var file_addition_addition_proto_depIdxs = []int32{
+var file_addition_proto_depIdxs = []int32{
 	0, // 0: addition.Addition.Add:input_type -> addition.AddRequest
 	1, // 1: addition.Addition.Add:output_type -> addition.AddResponse
 	1, // [1:2] is the sub-list for method output_type
@@ -176,13 +177,13 @@ var file_addition_addition_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_addition_addition_proto_init() }
-func file_addition_addition_proto_init() {
-	if File_addition_addition_proto != nil {
+func init() { file_addition_proto_init() }
+func file_addition_proto_init() {
+	if File_addition_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_addition_addition_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_addition_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AddRequest); i {
 			case 0:
 				return &v.state
@@ -194,7 +195,7 @@ func file_addition_addition_proto_init() {
 				return nil
 			}
 		}
-		file_addition_addition_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_addition_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AddResponse); i {
 			case 0:
 				return &v.state
@@ -211,18 +212,98 @@ func file_addition_addition_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_addition_addition_proto_rawDesc,
+			RawDescriptor: file_addition_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_addition_addition_proto_goTypes,
-		DependencyIndexes: file_addition_addition_proto_depIdxs,
-		MessageInfos:      file_addition_addition_proto_msgTypes,
+		GoTypes:           file_addition_proto_goTypes,
+		DependencyIndexes: file_addition_proto_depIdxs,
+		MessageInfos:      file_addition_proto_msgTypes,
 	}.Build()
-	File_addition_addition_proto = out.File
-	file_addition_addition_proto_rawDesc = nil
-	file_addition_addition_proto_goTypes = nil
-	file_addition_addition_proto_depIdxs = nil
+	File_addition_proto = out.File
+	file_addition_proto_rawDesc = nil
+	file_addition_proto_goTypes = nil
+	file_addition_proto_depIdxs = nil
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConnInterface
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion6
+
+// AdditionClient is the client API for Addition service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type AdditionClient interface {
+	Add(ctx context.Context, in *AddRequest, opts ...grpc.CallOption) (*AddResponse, error)
+}
+
+type additionClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewAdditionClient(cc grpc.ClientConnInterface) AdditionClient {
+	return &additionClient{cc}
+}
+
+func (c *additionClient) Add(ctx context.Context, in *AddRequest, opts ...grpc.CallOption) (*AddResponse, error) {
+	out := new(AddResponse)
+	err := c.cc.Invoke(ctx, "/addition.Addition/Add", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// AdditionServer is the server API for Addition service.
+type AdditionServer interface {
+	Add(context.Context, *AddRequest) (*AddResponse, error)
+}
+
+// UnimplementedAdditionServer can be embedded to have forward compatible implementations.
+type UnimplementedAdditionServer struct {
+}
+
+func (*UnimplementedAdditionServer) Add(context.Context, *AddRequest) (*AddResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Add not implemented")
+}
+
+func RegisterAdditionServer(s *grpc.Server, srv AdditionServer) {
+	s.RegisterService(&_Addition_serviceDesc, srv)
+}
+
+func _Addition_Add_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AddRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AdditionServer).Add(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/addition.Addition/Add",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AdditionServer).Add(ctx, req.(*AddRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Addition_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "addition.Addition",
+	HandlerType: (*AdditionServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Add",
+			Handler:    _Addition_Add_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "addition.proto",
 }
---
+++ simpleserver.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/byteshiva/simplegrpc/addition"
+
+	"google.golang.org/grpc"
+)
+
+type additionServer struct {
+}
+
+func (s *additionServer) Add(c context.Context, addRequest *addition.AddRequest) (*addition.AddResponse, error) {
+	result := addRequest.Number + addRequest.AnotherNumber
+	response := addition.AddResponse{
+		Sum: int64(result),
+	}
+	return &response, nil
+}
+
+func newAddServer() *additionServer {
+	return &additionServer{}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:10000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	addition.RegisterAdditionServer(grpcServer, newAddServer())
+	grpcServer.Serve(lis)
+}
